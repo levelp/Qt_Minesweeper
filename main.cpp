85b3c3ca31d4a4c4e431d2cc39ed264e82c6b6a8
Denis Stepulenok <super.denis@gmail.com>
2015-05-16 15:49:25 +0300

    Игра Сапёр на Qt5 - использование перевода

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+#include "Common.h"
+#include <QDebug>
+
+int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
+
+  Minner w;
+  w.show();
+
+  QTranslator trans;
+  QString locale = QLocale::system().name();
+  qDebug() << "locale:" << locale;
+  qDebug() << "Cur dir:" << QDir::currentPath();
+  trans.load("../minner_ru");
+  a.installTranslator(&trans);
+
+  return a.exec();
+}
